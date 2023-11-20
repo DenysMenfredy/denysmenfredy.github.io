ed06f4f53a6af2346c3dccea23a67b7371dfd9b6
Denys Menfredy <denys.menfredy7@gmail.com>
2023-11-20 20:06:00 -0300

    first nuxt3 template commit

---
+++ denysmenfredy/components/AppHeader.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <header class="grid grid-cols-2 bg-zinc-900">
+            <h1 class="inline-block text-2xl font-bold ml-8 text-amber-500 hover:text-amber-600">Denys Menfredy</h1>
+            <ul class="inline list-none space-x-4 justify-self-end mr-8">
+                <li class="float-left text-base font-medium text-slate-200 hover:text-slate-300">Home</li>
+                <li class="float-left text-base font-medium text-slate-200 hover:text-slate-300">About</li>
+                <li class="float-left text-base font-medium text-slate-200 hover:text-slate-300">Contact</li>
+                <li class="float-left text-base font-medium text-slate-200 hover:text-slate-300">Blog</li>
+            </ul>
+        </header>
+    </div>
+</template>
