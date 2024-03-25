6ea1fa2e95ecd2322a5d741b1c4e502fa7107d81
Denys Menfredy <denys.menfredy7@gmail.com>
2024-03-25 17:09:12 -0300

    working on blogging

---
+++ components/content/TextBlock.vue
@@ -0,0 +1,5 @@
+<template>
+    <p class="text-lg">
+        <ContentSlot :use="$slots.default" unwrap="p" />
+    </p>
+</template>
---
+++ layouts/blog.vue
@@ -0,0 +1,6 @@
+<template>
+    <section class="w-full bg-white flex justify-center">
+        <slot />
+    </section>
+    <AppFooter />
+</template>
