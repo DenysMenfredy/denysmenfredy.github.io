b69d9a02531aeaf0ee20e5766fa8bea60334c790
Denys Menfredy <denys.menfredy7@gmail.com>
2024-03-27 11:50:48 -0300

    ContentList

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -1,6 +1,10 @@
 <template>
     <main class="w-3/5">
-        <ContentDoc />
+        <ContentDoc>
+            <template #not-found>
+                <h1>Sorry, the content you're looking for doesn't exists yet... :/ </h1>
+            </template>
+        </ContentDoc>
     </main>
 </template>
 
